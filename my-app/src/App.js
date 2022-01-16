a732bdabb4f5cdee05fcacfdf6f0f269c455bf3c
renium77 <54940001+renium77@users.noreply.github.com>
2022-01-16 18:43:36 +0530

    initial push

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='test-3xl font-bold underline'>
+        hello World!
+      </div>
+    </div>
+  );
+}
+
+export default App;
